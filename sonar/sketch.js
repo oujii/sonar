2791a1ed91e6b0963f0910daf147d238f9f0424f
Carl Waerner <carl.waerner@gmail.com>
2018-07-08 21:43:19 +0200

    bajs

--- sonar/sketch.js
+++ sonar/sketch.js
@@ -1,12 +1,23 @@
 function setup() {
   // put setup code here
-  createCanvas(200, 100);
+  createCanvas(windowWidth, windowHeight);
+  fill(color(255, 204, 0));
+  pg = createGraphics(500, 500);
+  for (var i = 0; i < 5; i++) {
+    var x = random(windowWidth);
+    var y = random(windowHeight);
+
+    ellipse(x, y, 50, 50);
+  }
 }
 
 function draw() {
-  // put drawing code here
-  background(250);
-  fill(color(255, 204, 0));
-  noStroke();
-  
+  // ett objekt
+
+  pg.fill(200, 0, 200);
+
+  pg.rect(random(500), random(200), 200, 200);
+
+  image(pg, 0, 0);
+  pg.background(250);
 }
