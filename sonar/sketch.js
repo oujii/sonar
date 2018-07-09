4d73664800e48e73d5ce4efb6610070d4ec488ed
Carl Waerner <carl.waerner@gmail.com>
2018-07-09 15:58:38 +0200

    ifarna flyttad till MouseMoved samt 3 hitboxes

--- sonar/sketch.js
+++ sonar/sketch.js
@@ -1,6 +1,16 @@
 var circles = {};
+var playMode = "sustain";
+var tone1;
+var tone2;
+var tone3;
+var hitbox1 = 75;
+var hitbox2 = 50;
+var hitbox3 = 25;
 
 function setup() {
+  tone1 = loadSound("one.wav");
+  tone2 = loadSound("two.wav");
+  tone3 = loadSound("three.wav");
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
   noFill();
@@ -15,8 +25,6 @@ function setup() {
 }
 
 function draw() {
-  var hitbox = 25;
-
   stroke("red");
   ellipse(circles[0][0], circles[0][1], 50, 50);
   stroke("blue");
@@ -27,40 +35,63 @@ function draw() {
   ellipse(circles[3][0], circles[3][1], 50, 50);
   stroke("purple");
   ellipse(circles[4][0], circles[4][1], 50, 50);
+}
 
+function mouseMoved() {
   if (
-    mouseX > circles[0][0] - hitbox &&
-    mouseX < circles[0][0] + hitbox &&
-    mouseY > circles[0][1] - hitbox &&
-    mouseY < circles[0][1] + hitbox
+    mouseX > circles[0][0] - hitbox1 &&
+    mouseX < circles[0][0] + hitbox1 &&
+    mouseY > circles[0][1] - hitbox1 &&
+    mouseY < circles[0][1] + hitbox1
   ) {
-    console.log("*** 0 ***");
+    console.log("*** 0 *** - första tonen");
+    tone1.play();
+
+    if (
+      mouseX > circles[0][0] - hitbox2 &&
+      mouseX < circles[0][0] + hitbox2 &&
+      mouseY > circles[0][1] - hitbox2 &&
+      mouseY < circles[0][1] + hitbox2
+    ) {
+      console.log("*** 0 *** - andra tonen");
+      tone2.play();
+
+      if (
+        mouseX > circles[0][0] - hitbox3 &&
+        mouseX < circles[0][0] + hitbox3 &&
+        mouseY > circles[0][1] - hitbox3 &&
+        mouseY < circles[0][1] + hitbox3
+      ) {
+        console.log("*** 0 *** - tredje tonen");
+        tone3.play();
+      }
+    }
   } else if (
-    mouseX > circles[1][0] - hitbox &&
-    mouseX < circles[1][0] + hitbox &&
-    mouseY > circles[1][1] - hitbox &&
-    mouseY < circles[1][1] + hitbox
+    mouseX > circles[1][0] - hitbox1 &&
+    mouseX < circles[1][0] + hitbox1 &&
+    mouseY > circles[1][1] - hitbox1 &&
+    mouseY < circles[1][1] + hitbox1
   ) {
     console.log("*** 1 ***");
   } else if (
-    mouseX > circles[2][0] - hitbox &&
-    mouseX < circles[2][0] + hitbox &&
-    mouseY > circles[2][1] - hitbox &&
-    mouseY < circles[2][1] + hitbox
+    mouseX > circles[2][0] - hitbox1 &&
+    mouseX < circles[2][0] + hitbox1 &&
+    mouseY > circles[2][1] - hitbox1 &&
+    mouseY < circles[2][1] + hitbox1
   ) {
     console.log("*** 2 ***");
   } else if (
-    mouseX > circles[3][0] - hitbox &&
-    mouseX < circles[3][0] + hitbox &&
-    mouseY > circles[3][1] - hitbox &&
-    mouseY < circles[3][1] + hitbox
+    mouseX > circles[3][0] - hitbox1 &&
+    mouseX < circles[3][0] + hitbox1 &&
+    mouseY > circles[3][1] - hitbox1 &&
+    mouseY < circles[3][1] + hitbox1
   ) {
     console.log("*** 3 ***");
   } else if (
-    mouseX > circles[4][0] - hitbox &&
-    mouseX < circles[4][0] + hitbox &&
-    mouseY > circles[4][1] - hitbox &&
-    mouseY < circles[4][1] + hitbox
+    mouseX > circles[4][0] - hitbox1 &&
+    mouseX < circles[4][0] + hitbox1 &&
+    mouseY > circles[4][1] - hitbox1 &&
+    mouseY < circles[4][1] + hitbox1
   ) {
     console.log("*** 4 ***");
   }
